bd20c1ecc57e6de3af03c8f4c245a0812268ae5f
Meghan Hade <mmhade@gmail.com>
2014-09-05 16:31:59 -0700

    initial commit

---
+++ README.txt
@@ -0,0 +1,17 @@
+# Post box design implementation challenge
+
+## Prompt
+Given the `index.html`, `main.css`, and `main.js` files within this folder, implement the included UX designs found in `design.jpg` as closely as possible.
+
+## Hints
+Feel free to change or remove any files or code that we've included. You are especially encouraged to modify the main post box template found in `index.html` to suit your needs.
+
+You should be able to work through the entire implementation without working in `main.js`, but it may be helpful to look at the comments in the file to understand what is going on.
+
+## Success
+A successful implementation will look similar (or the same) as the designs, and will have the correct UX interactions as specified. These should be:
+
+    #. When the user loads the page, the post box should be minimized
+    #. When the user clicks into the post box, the submit button should appear and the post box should expand
+    #. When the user clicks submit, the post box should be replaced with the submitted text and a "Success" message should appear
+    #. The user should be able to click "Start Over" to return to the first step
---
+++ main.js
@@ -0,0 +1,45 @@
+$(function () {
+    'use strict';
+
+    var PostBoxView = Backbone.View.extend({
+
+        // Create a template function using Handlebars.js.
+        // See http://handlebarsjs.com/.
+        template: Handlebars.compile($('#post-box-tmpl').html().trim()),
+
+        events: {
+            // Whenenever the user clicks on the text area we will add an `expanded`
+            // class to the `textarea-wrapper` div.
+            'click textarea': function () { this.$('.textarea-wrapper').addClass('expanded'); },
+
+            // Whenever the user clicks on the submit button we will set the model's
+            // `text` attribute to whatever the user entered into the `textarea` element.
+            'click .textarea-wrapper button': function () { this.model.set('text', this.$('textarea').val()); }
+        },
+
+        initialize: function () {
+
+            // Create a blank view-model. We'll use this to respond save state
+            // whenever the UI changes. See http://addyosmani.com/blog/understanding-mvvm-a-guide-for-javascript-developers/
+            // for more context about view-models.
+            this.model = new Backbone.Model();
+
+            // Whenever the model's attribute change, we will re-render the entire
+            // post box view. This happens automatically using Backbone's event
+            // callback infastructure http://backbonejs.org/#Events.
+            this.listenTo(this.model, 'change', this.render);
+        },
+
+        render: function () {
+            console.log('Rendering the post box.');
+
+            // Pass the model's attribute to the post box template.
+            this.$el.html(this.template(this.model.toJSON()));
+            return this;
+        }
+
+    });
+
+    var postBox = new PostBoxView();
+    $('#main').html(postBox.render().el);
+});
